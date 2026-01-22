cc483c9a07ad85820d0860def1057a2b0f82eb1c
mamidisupriyasri2006-netizen <mamidisupriyasri2006@gmail.com>
2026-01-22 10:47:00 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,16 @@
+let marks=90
+if(marks>=50)
+{
+    console.log("your grade is A");
+}
+else if(marks>=85)
+{
+    console.log("your grade is B");
+}
+else if(marks>=60)
+{
+    console.log("your grade is C");
+} 
+else {
+    console.log("you are failed!!");
+}
